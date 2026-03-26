0101f5e95cb0b9e8034ff8dbb2cdb41e004ca344
Ahsan Alam <ahsanulalam.500@gmail.com>
2026-03-27 03:32:12 +0600

    skill page section updated

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -3,6 +3,7 @@ import Experience from "../components/home/Experience";
 export default function page() {
   return (
     <>
+      <div className="mt-[-30px]"></div>
       <Experience />
     </>
   );
