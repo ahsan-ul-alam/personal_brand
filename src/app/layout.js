1fd667233549fecbc3298d5234d8654ec9f3cb23
Ahsan Alam <ahsanulalam.500@gmail.com>
2026-03-27 03:49:20 +0600

    metadata and og updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,40 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Ahsan Alam",
-  description: "Your Best Developer is Here",
+  title: {
+    default: "Ahsan Alam | Full Stack Web Developer",
+    template: "%s | Ahsan Alam",
+  },
+  description:
+    "I build modern, scalable, and high-performance websites and web applications using React, Next.js, PHP, and Laravel.",
+
+  metadataBase: new URL("https://ahsanalam.online"),
+
+  openGraph: {
+    title: "Ahsan Alam | Full Stack Web Developer",
+    description:
+      "I build modern, scalable, and high-performance websites and web applications.",
+    url: "https://ahsanalam.online",
+    siteName: "Ahsan Alam Portfolio",
+    images: [
+      {
+        url: "/ahsan_alam.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Ahsan Alam Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Ahsan Alam | Full Stack Developer",
+    description:
+      "Full Stack Developer specializing in React, Next.js & Laravel.",
+    images: ["/ahsan_alam.jpg"],
+  },
 };
 
 export default function RootLayout({ children }) {
