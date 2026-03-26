15746ed85ca2047a43a83a7743e3da00045ca081
Ahsan Alam <ahsanulalam.500@gmail.com>
2026-03-26 23:50:04 +0600

    footer link fix

--- src/app/components/shared/Footer.jsx
+++ src/app/components/shared/Footer.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 const links = ["Skills", "Portfolio", "Contact"];
 
 const socials = [
@@ -78,13 +80,13 @@ export default function Footer() {
         {/* Nav Links */}
         <nav className="flex flex-wrap justify-center gap-x-6 gap-y-2">
           {links.map((link) => (
-            <a
+            <Link
               key={link}
               href={`${link.toLowerCase()}`}
               className="text-sm text-gray-400 hover:text-white transition-colors duration-150"
             >
               {link}
-            </a>
+            </Link>
           ))}
         </nav>
 
